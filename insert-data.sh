961eaa5e2c4ffca6f43c5bedb8da0df61b1a42e7
Felipe Apolinário <100887313+Fadokkx@users.noreply.github.com>
2024-05-22 00:37:44 -0300

    first commit

---
+++ insert-data.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# if f [[ $1 == "test" ]]
+# then
+  #PSQL="psql --username=postgres --dbname=cars -t --no-align -c"
+#else
+  #PSQL="psql --username=postgres --dbname=cars -t --no-align -c"
+#fi
+#$PSQL "TRUNCATE customers, cars"
+
+#cat cars_data_text.csv | while IFS="," read -r YEAR_MODEL MAKE MODEL SIZE PLATE
---
+++ script.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import psycopg2
+
+try:
+    # CONECT INTO POSTGRESQL 
+    conn = psycopg2.connect(
+        host="localhost",
+        database="cars",
+        user="postgres",
+        password="123",
+        options="-c client_encoding=UTF8"
+    )
+
+    # CREATE A CURSOR
+    cursor = conn.cursor()
+
+    # COUNT THE CARS BY YEAR INTO THE DATABASE 
+    cursor.execute("SELECT year_model, COUNT(*) FROM cars GROUP BY year_model ORDER BY year_model")
+
+    # SEARCH ALL CONSULTS RESULTS
+    rows = cursor.fetchall()
+
+    # QUIT CURSOR AND
+    cursor.close()
+    conn.close()
+
+    # EXTRACT YEAR AND COUNT IN CARS
+    year = [row[0] for row in rows]
+    count = [row[1] for row in rows]
+
+    # Criar o gráfico de pizza
+    plt.figure(figsize=(8, 6))
+    plt.pie(count, labels=year, autopct='%1.1f%%', startangle=140)    
+    plt.title('Cars per year')
+    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    # Mostrar o gráfico
+    plt.show()
+
+except psycopg2.Error as e:
+    print("Erro ao conectar ao PostgreSQL:", e)
